8eaf606d0fd925ba1df43ecfd7ad1f142f7b3611
Aditi Mishra <asmishra93@gmail.com>
2018-04-07 20:14:20 -0500

    more functional

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var router = require('express').Router();
 var dataRouter = require('./apiRoutes');
-//var htmlRouter = require('./htmlRoutes');
+var htmlRouter = require('./htmlRoutes');
 
 router.use('/api/articles', dataRouter);
-//router.use('/', htmlRouter);
+router.use('/', htmlRouter);
 
 module.exports = router;
 
